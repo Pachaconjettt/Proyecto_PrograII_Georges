f3d5fcb20fd5f990170ee077eb0acbcc7018db1b
Pachaconjettt <efrain.retana.segura@est.una.ac.cr>
2025-04-15 04:50:28 -0600

    Cuarto dia, ya casi a terminar

--- Biblioteca.cpp
+++ Biblioteca.cpp
@@ -24,7 +24,6 @@ void Biblioteca::setNombre(string nom) {this->nombre = nom;}
 Biblioteca::~Biblioteca() {
 
 }
-
 void Biblioteca::menu() {
     bool yes = true;
     int opcion;
@@ -34,49 +33,49 @@ void Biblioteca::menu() {
     system("cls");
 
     cout << *currentDate << endl;
-    cout << "\nBienvenido a la Biblioteca -[ "  << nombre << " ]-" << endl <<endl;
+    cout << "\nBienvenido a la Biblioteca -[ "  << nombre << " ]-" << endl << endl;
     system("pause");
-    do {
-        do {
-            system("cls");
-            cout << "\t     Menu Principal" << endl;
-            cout << "\n_._._._._._._._._._._._._._._._._._._._" << endl;
-            cout << "\n1. Inclusión de datos de materiales (libros, revistas, artículos, videos, material digital)" << endl;
-            cout << "\n2. Modificación de datos de materiales" << endl;
-            cout << "\n3. Inclusión de usuarios" << endl;
-            cout << "\n4. Modificación de datos de usuarios" << endl;
-            cout << "\n5. Registro de solicitudes de préstamo y devoluciones" << endl;
-            cout << "\n6. Reporte de inventario de materiales" << endl;
-            cout << "\n7. Reporte de usuarios" << endl;
-            cout << "\n8. Reporte de materiales en préstamo (general y por tipo de material)" << endl;
-            cout << "\n9. Reporte de préstamos por usuario" << endl;
-            cout << "\n_._._._._._._._._._._._._._._._._._._._\n" << endl;
-            cout << "\nDigite una opcion: ";
-            cin >>opcion;
-            try {
-                IntOutRange(1,9, opcion, yes);
-            }
-            catch (invalid_argument &e) {
-                system("cls");
-                cout << e.what()<< "\n" << endl;
-                system("pause");
-                yes = false;
-            }
-        }while ((opcion < 1 || opcion > 6 ) && yes == false);
 
-    system("cls");
-    switch (opcion) {
-        case 1: InclusionDatosMats(); break;
+    do {
+        system("cls");
+        cout << "\t     Menu Principal" << endl;
+        cout << "\n_._._._._._._._._._._._._._._._._._._._" << endl;
+        cout << "\n1. Inclusión de datos de materiales (libros, revistas, artículos, videos, material digital)" << endl;
+        cout << "\n2. Modificación de datos de materiales" << endl;
+        cout << "\n3. Inclusión de usuarios" << endl;
+        cout << "\n4. Modificación de datos de usuarios" << endl;
+        cout << "\n5. Registro de solicitudes de préstamo y devoluciones" << endl;
+        cout << "\n6. Reporte de inventario de materiales" << endl;
+        cout << "\n7. Reporte de usuarios" << endl;
+        cout << "\n8. Reporte de materiales en préstamo (general y por tipo de material)" << endl;
+        cout << "\n9. Reporte de préstamos por usuario" << endl;
+        cout << "\n_._._._._._._._._._._._._._._._._._._._\n" << endl;
+        cout << "\nDigite una opcion: ";
+        cin >> opcion;
+
+        try {
+            IntOutRange(1, 9, opcion, yes);
+        } catch (invalid_argument &e) {
+            system("cls");
+            cout << e.what() << "\n" << endl;
+            system("pause");
+            continue; // Reinicia el bucle
         }
-    }while (opcion != 6);
+
+        system("cls");
+        switch (opcion) {
+            case 1:
+                InclusionDatosMats();
+            break;
+        }
+    } while (opcion != 6);
 }
 
 void Biblioteca::InclusionDatosMats() {
-system("cls");
+    system("cls");
     cout << "Ingresa el nombre del material" << endl;
-
-
 }
+
 
 
 
